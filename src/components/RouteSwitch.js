31f4e235791e5a8270d22f1f9dae2bcbc4a1d171
Chase Lukasiewicz <chaseluka@gmail.com>
2022-10-07 13:04:19 -0600

    fix

--- src/components/RouteSwitch.js
+++ src/components/RouteSwitch.js
@@ -1,18 +1,18 @@
 import Homepage from "./Homepage";
 import Nav from "./Nav";
 import Shop from "./Shop";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import React from "react";
 
 const RouteSwitch = () => {
   return (
-    <Router basename={process.env.PUBLIC_URL + "/"}>
+    <BrowserRouter basename={process.env.PUBLIC_URL + "/"}>
       <Nav />
       <Routes>
         <Route path="/" element={<Homepage />} />
         <Route path="/shop" element={<Shop />} />
       </Routes>
-    </Router>
+    </BrowserRouter>
   );
 };
 
